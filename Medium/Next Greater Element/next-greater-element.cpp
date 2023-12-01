9a72600a8d21d8d223041ad2d7ca42de147e358d
Abhinav Dubey <118627945+itsabhi-nav@users.noreply.github.com>
2023-12-01 07:19:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Next Greater Element/next-greater-element.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n) {
+        stack<long long> st;
+        vector<long long> v1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            // Pop elements from the stack until the top is greater than arr[i]
+            while (!st.empty() && st.top() <= arr[i]) {
+                st.pop();
+            }
+
+            // If stack is empty, no greater element found
+            if (st.empty()) {
+                v1.push_back(-1);
+            } else {
+                v1.push_back(st.top());
+            }
+
+           
+            st.push(arr[i]);
+        }
+
+        // Reverse the result vector to get the final answer
+        reverse(v1.begin(), v1.end());
+        return v1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
