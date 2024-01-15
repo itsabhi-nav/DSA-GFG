c23db2a3dbc03fb21e23374ddc9f8d9702a8c379
Abhinav Dubey <118627945+itsabhi-nav@users.noreply.github.com>
2024-01-16 02:48:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -10,18 +10,59 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
+		
+		int firstOccurence(int arr[], int n, int x){
+		    int first =0;
+		    int last =n-1;
+		    int res =-1;
+		    
+		    while(first<=last){
+		        int mid = (first+(last-first)/2);
+		        if(arr[mid]==x){
+		            res=mid;
+		            last =mid-1;
+		        }
+		        
+		        else if(arr[mid]>x)
+		        last =mid-1;
+		        
+		        else
+		        first =mid+1;
+		    }
+		    return res;
+		}
+		
+		int lastOccurence(int arr[], int n, int x){
+		    int first =0;
+		    int last =n-1;
+		    int res =-1;
+		    
+		    while(first<=last){
+		        int mid = (first+(last-first)/2);
+		        if(arr[mid]==x){
+		            res=mid;
+		            first=mid+1;
+		        }
+		        
+		        else if(arr[mid]>x)
+		        last =mid-1;
+		        
+		        else
+		        first =mid+1;
+		    }
+		    return res;
+		}
+		
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int count =0;
-	    
-	    for(int i =0; i<n; i++)
-	    {
-	        if(x==arr[i]){
-	            count++;
-	        }
-	    }
-	    return count;
-	    
+        vector<int>v;
+        int first = firstOccurence(arr,n,x);
+        int last = lastOccurence(arr,n,x);
+        
+         if (first == -1)
+            return 0; // Element not found
+        else
+            return last - first + 1;
 	    
 	}
 };
