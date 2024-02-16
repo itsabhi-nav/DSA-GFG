a20959048f4e35a653602bdb325f36e68f013c49
Abhinav Dubey <118627945+itsabhi-nav@users.noreply.github.com>
2024-02-16 20:13:56 +0530

    Updated Solution - GfG to GitHub

--- Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -12,24 +12,27 @@ class Solution
     bool ispar(string str)
     {
         // Your code here
-        stack<int>s;
-        for (int i =0; i<str.length(); i++){
-            if(str[i]=='(' || str[i]=='{' || str[i]=='[')
-            s.push(str[i]);
-            else{
-                if(s.empty()==true)
-                return false;
-                else if (matching(s.top(), str[i])==false)
+        stack<char>s;
+        
+        for(int i =0; i<str.size(); i++){
+            if(str[i]=='('|| str[i]=='{' || str[i]=='['){
+                s.push(str[i]);
+            }
+            else {
+                if(s.empty())
                 return false;
                 else
+                if(( s.top()=='(' && str[i]==')' ) || (s.top()=='{' && str[i]=='}') || (s.top()=='[' && str[i]==']'))
                 s.pop();
-            }
+                else
+                return false;
+            } 
+           
         }
-        return (s.empty()==true);
-    }
-    
-    bool matching(char a, char b){
-        return((a=='('&& b==')') || (a=='{'&& b=='}') || (a=='['&& b==']'));
+        if(s.empty())
+        return true;
+        else
+        return false;
     }
 
 };
