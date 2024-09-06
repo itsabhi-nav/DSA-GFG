e9deebe4e1b573241097cb16b5eb9a1c5c98244b
Abhinav Dubey <118627945+itsabhi-nav@users.noreply.github.com>
2024-09-06 23:15:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -9,18 +9,22 @@ using namespace std;
 class Solution {
   public:
     // Function to find the sum of contiguous subarray with maximum sum.
-    long long maxSubarraySum(vector<int> &arr) {
-        // code here...
-        int res = arr[0];
-        int max_ending = arr[0];
+    int maxSubarraySum(vector<int> &arr) {
         int n = arr.size();
-        
-        for(int i =1; i<n ; i++){
-            max_ending= max(max_ending+ arr[i], arr[i]);
-            res = max(res, max_ending);
-        }
-        return res;
-        
+          // Your code here
+            if (n == 0) {
+        return 0;
+    }
+
+    long long max_sum = arr[0];
+    long long current_sum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        current_sum = max((long long)arr[i], current_sum + arr[i]);
+        max_sum = max(max_sum, current_sum);
+    }
+
+    return max_sum;
     }
 };
 
